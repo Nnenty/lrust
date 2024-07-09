34480ed494654a238a8d7b51e602653dcd1f69cd
Nnenty <>
2024-07-09 21:06:16 +0300

    add chapter 1

--- async_book/ch_1/lrust/src/main.rs
+++ async_book/ch_1/lrust/src/main.rs
@@ -1,3 +1,22 @@
+use futures::executor::block_on;
+
+async fn meow() {
+    println!("meow")
+}
+async fn hello_during_sleep() {
+    meow().await;
+    sleep().await;
+}
+async fn sleep() {
+    println!("sleeping");
+}
+async fn meow_hello() {
+    futures::join!(hello_during_sleep(), hello());
+}
+async fn hello() {
+    println!("hello")
+}
+
 fn main() {
-    println!("Hello, world!");
+    block_on(meow_hello());
 }
