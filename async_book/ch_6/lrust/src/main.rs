040b449f397008fd47300371ca6c6d14fcb9c51b
Nnenty <>
2024-07-13 11:22:56 +0300

    add new module

--- async_book/ch_6/lrust/src/main.rs
+++ async_book/ch_6/lrust/src/main.rs
@@ -1,10 +1,12 @@
-use futures::{self, executor::block_on, join, try_join};
+use futures::executor::block_on;
 
-use join_macroc::{pure_join::get_music_and_book, try_join::try_get_music_and_book};
-mod join_macroc {
-    use super::*;
+use join_macro::{pure_join::get_music_and_book, try_join::try_get_music_and_book};
+mod join_macro {
+    use futures;
 
     pub mod pure_join {
+        use super::*;
+
         async fn get_music() {
             println!("get music");
         }
@@ -34,7 +36,65 @@ mod join_macroc {
             let get_b = try_get_book();
 
             // will async execute
-            try_join!(get_m, get_b).unwrap();
+            futures::try_join!(get_m, get_b).unwrap();
+        }
+    }
+}
+
+use select_macro::{default_and_complete_branches::sum_with_select, simple_use::select_task};
+mod select_macro {
+    pub mod simple_use {
+        use futures::{future::FutureExt, pin_mut, select};
+
+        async fn first_task() {
+            println!("first task")
+        }
+        async fn second_task() {
+            println!("second task")
+        }
+
+        pub async fn select_task() {
+            let t1 = first_task().fuse();
+            let t2 = second_task().fuse();
+
+            pin_mut!(t1, t2);
+
+            select! {
+                () = t2 => {
+                    println!("second task ends first");
+                },
+                () = t1 => {
+                    println!("first task ends first");
+                }
+            }
+        }
+    }
+
+    pub mod default_and_complete_branches {
+        use futures::{future, select};
+
+        pub async fn sum_with_select() {
+            let mut fut1 = future::ready(2);
+            let mut fut2 = future::ready(2);
+
+            let mut sum = 0;
+
+            loop {
+                select! {
+                    a = fut1 => {
+                        sum += a;
+                    },
+                    b = fut2 => {
+                        sum += b;
+                    },
+                    complete => break,
+                    default => unreachable!(),
+                };
+            }
+
+            assert_eq!(sum, 4);
+
+            println!("sum = {sum}");
         }
     }
 }
@@ -42,7 +102,15 @@ fn main() {
     // Modules in programm:
     //
     // 1. mod join_macro
+    // 2. mod select_macro
 
-    block_on(get_music_and_book());
-    block_on(try_get_music_and_book());
+    // functions from join_macro module:
+    //
+    // block_on(get_music_and_book());
+    // block_on(try_get_music_and_book());
+
+    // functions from select_macro module:
+    //
+    block_on(select_task());
+    block_on(sum_with_select());
 }
