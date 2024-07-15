9610b59ea6bc688ca5defc64f6e2494a417c9baa
Nnenty <>
2024-07-15 11:15:24 +0300

    add module.patch

--- async_book/ch_7/lrust/src/main.rs
+++ async_book/ch_7/lrust/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use futures;
+
+mod question_mark_async {
+    use std::io;
+
+    // type annotations needed
+    pub async fn nono() {
+        let fut = async {
+            // foo().await?;
+
+            // Ok(())
+        };
+    }
+    async fn foo() -> Result<(), io::Error> {
+        Ok(())
+    }
+
+    pub async fn yesyes() {
+        let fut = async {
+            foo().await?;
+
+            // explicit type conversion
+            Ok::<(), io::Error>(())
+        };
+    }
 }
+mod send_approximation {}
+fn main() {}
