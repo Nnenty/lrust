b56546c67ec17e15f3da59709d2f0a1cce7ffb43
Nnenty <frag1337n@gmail.com>
2024-07-15 13:16:56 +0300

    add final recursive module

--- async_book/ch_7/lrust/src/main.rs
+++ async_book/ch_7/lrust/src/main.rs
@@ -1,4 +1,5 @@
-use futures;
+use async_recursive::recursive;
+use futures::{self, executor::block_on};
 
 mod question_mark_async {
     use std::io;
@@ -31,7 +32,9 @@ mod send_approximation {
     use std::rc::Rc;
 
     #[derive(Default)]
-    struct NotSend(Rc<()>);
+    struct NotSend {
+        _r: Rc<()>,
+    }
 
     async fn foo() {}
 
@@ -44,25 +47,43 @@ mod send_approximation {
     // try paste this func in argument of function `expected_send()`
     // to see error:
     pub async fn _bad_use_notsend() {
-        let _ns = NotSend { 0: Rc::new(()) };
+        let _ns = NotSend { _r: Rc::new(()) };
 
         foo().await;
     }
 
     pub async fn good_use_notsend() {
         {
-            let _ns = NotSend { 0: Rc::new(()) };
+            let _ns = NotSend { _r: Rc::new(()) };
         }
 
         foo().await;
     }
     pub fn expected_send(_: impl Send) {}
 }
+
+mod async_recursive {
+    use futures::future::BoxFuture;
+    use futures::FutureExt;
+
+    pub fn recursive(i: u8) -> BoxFuture<'static, ()> {
+        async move {
+            println!("{i} hello!");
+
+            if i < 5 {
+                recursive(i + 1).await;
+            }
+        }
+        .boxed()
+    }
+}
 fn main() {
     // Modules in programm:
     //
     // 1. mod question_mark_async
     // 2. mod send_approximation
-    //
+    // 3. mod async_recursive
+
     expected_send(good_use_notsend());
+    block_on(recursive(1));
 }
